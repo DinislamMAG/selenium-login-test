c19b352ad583c575aa8d4ef6fc12865361eb9f53
DinislamMAG <dbog33@mail.ru>
2026-05-14 21:20:55 +0500

    remove target folder

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 
+
 public class LoginTest {
     public static void main(String[] args) {
         {
--- src/test/java/NegaTestLogin.java
+++ src/test/java/NegaTestLogin.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.junit.jupiter.api.Test;
 
 public class NegaTestLogin {
 
